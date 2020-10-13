8b961509ce7003bdb1486f22905ae1bec3db759f
Bhavana123sai <69452550+Bhavana123sai@users.noreply.github.com>
2020-10-13 11:52:39 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,13 @@
+n=5
+for i in range(n):
+    for k in range(n,i,-1):
+        print('* ',end="")
+    for j in range(1,i+1):
+        print(j,end=" ")
+    for l in range(0,i+1):
+        print(l,end=" ")
+    print("")
+
+                   
+    
+   
