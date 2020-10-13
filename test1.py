0d659fcba110bf21fe2817137b41e3923e5f5ee6
Bhavana123sai <69452550+Bhavana123sai@users.noreply.github.com>
2020-10-13 11:51:42 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,12 @@
+def digitalRoot(n): 
+    if (n == "0"): 
+        return 0
+    ans = 0
+    for i in range (0, len(n)): 
+        ans = (ans + int(n[i])) % 9
+    if(ans == 0): 
+        return 9
+    else:  
+        return ans % 9
+a=digitalRoot(str(1))
+print(a)
